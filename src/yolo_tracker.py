f14379b33b2eaa2c502eda65a5db374d742cd447
Ryosuke Hara <92095956+hara82@users.noreply.github.com>
2024-10-26 18:43:02 +0900

    manage dependencies and also add python code for tracking objects.

---
+++ src/yolo_tracker.py
@@ -0,0 +1,41 @@
+# Sample code to track objects in a video using YOLO11
+import cv2
+import logging
+
+from ultralytics import YOLO
+
+logger = logging.getLogger(__name__)
+
+# Load the YOLO11 model
+model = YOLO("yolo11n.pt")
+
+# Open the video file
+video_path = "../Whiplash.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLO11 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+        logger.warning(results)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLO11 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
