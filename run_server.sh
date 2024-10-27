e238d69afe5e8caa1925f3dc423802afd39af874
Ryosuke Hara <hara-ryosuke573@g.ecc.u-tokyo.ac.jp>
2024-10-27 13:20:20 +0900

    update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-deactivate
-cd /home/
-flask run --host=0.0.0.0 --port 5000
+cd /home/jphack24/tk_2420
+source .venv/bin/activate
+cd  ./project2
+
+if lsof -i :5000; then
+    echo "Port 5000 is already in use"
+else
+    flask run --host=0.0.0.0 --port 5000
+fi
