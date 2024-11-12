a46192f1ed76ec3ea6cbba4104945c01cf1c9b3a
Ryosuke Hara <92095956+hara82@users.noreply.github.com>
2024-11-12 16:54:31 +0900

    add recommendation page

---
+++ TS/oshinoko/app/recommendation/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/app/ui/Header';
+
+const RecommendationPage = () => {
+  return (
+    <div>
+      <Header />
+      <div>Recommendation Page</div>
+    </div>
+  );
+};
+
+export default RecommendationPage;
