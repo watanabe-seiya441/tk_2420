b64d5aeb9a100d5615cfcd3f9818a0c0e9ed3e23
Ryosuke Hara <92095956+hara82@users.noreply.github.com>
2024-11-15 23:55:01 +0900

    change path

--- TS/oshinoko/app/lib/config.ts
+++ TS/oshinoko/app/lib/config.ts
@@ -2,4 +2,5 @@ export const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export const annotationUrlPrefix = '/api/v1/annotations';
 export const memberProfileUrlPrefix = '/api/v1/member_profiles';
+export const mlModelUrlPrefix = '/api/v1/ml_models';
 export const videoUrlPrefix = '/api/v1/videos';
