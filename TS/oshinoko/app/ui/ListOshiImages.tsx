d580446ae0da898b8c669f9c36f43e6abe3e6e39
watanabe <watanabe-seiya441@g.ecc.u-tokyo.ac.jp>
2024-11-16 00:03:32 +0900

    パス変更の対応

--- TS/oshinoko/app/ui/ListOshiImages.tsx
+++ TS/oshinoko/app/ui/ListOshiImages.tsx
@@ -32,7 +32,7 @@ const ListOshiImages: React.FC<ListOshiImageProps> = ({
     const fetchMembers = async () => {
       try {
         const response = await axios.get(
-          `${backendUrl}/api/annotation_labels`,
+          `${backendUrl}/api/v1/member_profiles`,
           {
             params: { groupName }, // groupNameをリクエストパラメータとして使用
           },
@@ -40,10 +40,11 @@ const ListOshiImages: React.FC<ListOshiImageProps> = ({
         const data = response.data;
         console.log('Fetched data:', data);
 
-        // `label_name` をメンバーとして抽出
+        // `name` をメンバーとして抽出
         const extractedMembers = data.map(
-          (label: { label_name: string }) => label.label_name,
+          (profile: { name: string }) => profile.name,
         );
+
         console.log('Fetched members:', extractedMembers);
         setMembers(extractedMembers);
       } catch (err) {
@@ -61,7 +62,7 @@ const ListOshiImages: React.FC<ListOshiImageProps> = ({
     const fetchImagesForMember = async (member: string) => {
       try {
         const response = await axios.post<string[]>(
-          `${backendUrl}/api/list_oshi_images`,
+          `${backendUrl}/api/list_oshi_photos`,
           {
             videoTitle,
             member,
