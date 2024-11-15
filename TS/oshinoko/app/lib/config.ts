78753b66fb94020883b846d1badb94bbc96ea1a4
Ryosuke Hara <92095956+hara82@users.noreply.github.com>
2024-11-15 23:15:05 +0900

    fix frontend based on backend rename

--- TS/oshinoko/app/lib/config.ts
+++ TS/oshinoko/app/lib/config.ts
@@ -1,4 +1,5 @@
 export const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-export const videoUrlPrefix = '/api/v1/videos';
 export const annotationUrlPrefix = '/api/v1/annotations';
+export const memberProfileUrlPrefix = '/api/v1/member_profiles';
+export const videoUrlPrefix = '/api/v1/videos';
