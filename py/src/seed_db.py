73cfb4431de79cf7613befbe38837e9ce5f0280f
yagiryo <yagi-ryo143@g.ecc.u-tokyo.ac.jp>
2024-11-17 07:33:20 +0900

    update_db

--- py/src/seed_db.py
+++ py/src/seed_db.py
@@ -40,14 +40,25 @@ def seed_data():
         MemberProfile(group_member_id=1, name="karina", associated_color="blue", group_name="aespa"),
         MemberProfile(group_member_id=2, name="ningning", associated_color="purple", group_name="aespa"),
         MemberProfile(group_member_id=3, name="winter", associated_color="lightgreen", group_name="aespa"),
-        MemberProfile(group_member_id=0, name="天海祐希", associated_color="red", group_name="takarazuka"),
+        MemberProfile(group_member_id=0, name="hoshikaze_madoka", associated_color="red", group_name="takarazuka"),
+        MemberProfile(group_member_id=1, name="hosizora_misaki", associated_color="blue", group_name="takarazuka"),
+        MemberProfile(group_member_id=2, name="minami_maito", associated_color="pink", group_name="takarazuka"),
+        MemberProfile(group_member_id=3, name="oto_kurisu", associated_color="green", group_name="takarazuka"),
+        MemberProfile(group_member_id=4, name="seino_asuka", associated_color="yellow", group_name="takarazuka"),
+        MemberProfile(group_member_id=5, name="towaki_sea", associated_color="purple", group_name="takarazuka"),
+        MemberProfile(group_member_id=6, name="yuzuka_rei", associated_color="black", group_name="takarazuka"),
+        MemberProfile(group_member_id=0, name="iroha", associated_color="blue", group_name="illit"),
+        MemberProfile(group_member_id=1, name="minju", associated_color="pink", group_name="illit"),
+        MemberProfile(group_member_id=2, name="moka", associated_color="purple", group_name="illit"),
+        MemberProfile(group_member_id=3, name="wonhee", associated_color="lightgreen", group_name="illit"),
+        MemberProfile(group_member_id=4, name="yunah", associated_color="red", group_name="illit"),
     ]
 
     # データベースをクリアしてからデータを挿入
     with app.app_context():
         db.drop_all()
         db.create_all()
-        db.session.bulk_save_objects(videos)
+        # db.session.bulk_save_objects(videos)
         db.session.bulk_save_objects(member_profiles)
         db.session.commit()
         print("Database seeded successfully.")
